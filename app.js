49b33e3a04403f2d431317c22351d56374fde638
Robin van Steenbergen <robinvansteenbergen@hotmail.com>
2022-01-18 20:11:20 +0100

    all the changes

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('yo');
+let count = 0;
+function increment() {
+    count = count + 1
+    let countEl = document.getElementById('count-el');
+    countEl.innerText = count;
+
+}
+function save() {
+    console.log(count)
+}
+save();
