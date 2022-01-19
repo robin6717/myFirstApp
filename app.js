70708e053975e386dbe85ac29703c9a80c6db0c8
robin van steenbergen <robinvansteenbergen@hotmail.com>
2022-01-19 12:19:36 +0100

    changes

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-let count = 0;
+/*let count = 0;
 function increment() {
-    count = count + 1
+    count += 1
     let countEl = document.getElementById('count-el');
     countEl.innerText = count;
 
 }
+
+
+
 function save() {
-    console.log(count)
+    let saveCount = count + ' - '
+    let saveEl = document.getElementById('save-el');
+    saveEl.innerText += saveCount;
 }
-save();
+save();*/
+
+
+let countEl = document.getElementById('count-el');
+let count = 0;
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let saveEl = document.getElementById('save-el');
+    let saveCount = count + ' - '
+    saveEl.textContent += saveCount
+    count = 0
+    countEl.textContent = 0;
+}
